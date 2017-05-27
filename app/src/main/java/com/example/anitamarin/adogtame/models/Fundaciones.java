cc4a02e5dd575cdfcaf4c4d04aafab635fa41f90
Ana Marin <anamarin@unicauca.edu.co>
2017-05-27 17:42:22 -0500

    Detalle y template catalogo listos

--- app/src/main/java/com/example/anitamarin/adogtame/models/Fundaciones.java
+++ app/src/main/java/com/example/anitamarin/adogtame/models/Fundaciones.java
@@ -5,4 +5,63 @@ package com.example.anitamarin.adogtame.models;
  */
 
 public class Fundaciones {
+    String nombre, descripcion, direccion, imagen, horario;
+    int telefono;
+
+    public Fundaciones(String nombre, String descripcion, String direccion, String imagen, String horario, int telefono) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.direccion = direccion;
+        this.imagen = imagen;
+        this.horario = horario;
+        this.telefono = telefono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
 }
--- app/src/main/java/com/example/anitamarin/adogtame/models/Mascotas.java
+++ app/src/main/java/com/example/anitamarin/adogtame/models/Mascotas.java
@@ -5,4 +5,81 @@ package com.example.anitamarin.adogtame.models;
  */
 
 public class Mascotas {
+    String nombre, descripcion, fundacion, tamanio, edad, raza, imagen;
+    int contacto;
+
+    public Mascotas(String nombre, String descripcion, String fundacion, String tamanio, String edad, String raza, String imagen, int contacto) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.fundacion = fundacion;
+        this.tamanio = tamanio;
+        this.edad = edad;
+        this.raza = raza;
+        this.imagen = imagen;
+        this.contacto = contacto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getFundacion() {
+        return fundacion;
+    }
+
+    public void setFundacion(String fundacion) {
+        this.fundacion = fundacion;
+    }
+
+    public String getTamanio() {
+        return tamanio;
+    }
+
+    public void setTamanio(String tamanio) {
+        this.tamanio = tamanio;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public int getContacto() {
+        return contacto;
+    }
+
+    public void setContacto(int contacto) {
+        this.contacto = contacto;
+    }
 }
