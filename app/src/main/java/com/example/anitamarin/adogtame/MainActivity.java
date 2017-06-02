710bb05a59db4925da7cad3ad42f5020e8f83a48
Ana Marin <anamarin@unicauca.edu.co>
2017-06-02 09:23:32 -0500

    Tabs implementadas sin fragments

--- app/src/main/java/com/example/anitamarin/adogtame/MainActivity.java
+++ app/src/main/java/com/example/anitamarin/adogtame/MainActivity.java
@@ -1,13 +1,45 @@
 package com.example.anitamarin.adogtame;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.anitamarin.adogtame.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_catalogo:
+
+                    return true;
+                case R.id.navigation_donaciones:
+
+                    return true;
+                case R.id.navigation_seguimiento:
+
+                    return true;
+                case R.id.navigation_voluntariado:
+                    return true;
+            }
+            return false;
+        }
+
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        binding.bottomNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 }
